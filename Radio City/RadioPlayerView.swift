1519e461ef85ed53b9e55b1e574910d9e3cf17c7
Praveen <pravin17.sh@gmail.com>
2025-06-24 14:08:37 +0530

    Add 20 Radio Station

--- Radio City/RadioPlayerView.swift
+++ Radio City/RadioPlayerView.swift
@@ -13,14 +13,13 @@ import ModernSlider
 
 struct RadioPlayerView: View {
     
-    @StateObject private var playerManager = RadioPlayerManager()
+    @StateObject private var playerManager = RadioPlayerManager("")
     @State private var isPlaying = false
-    @State private var player: AVPlayer?
-    @State private var volume: Double = 0.5
-    
+    @State private var volume: Double = 0.9
     @State private var currentTime = Self.getCurrentTime()
-    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var currentFM = "Radio Suno Melody"
     
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
         VStack(spacing: 20) {
@@ -36,20 +35,20 @@ struct RadioPlayerView: View {
                 .background(Color.white.opacity(0.2))
                 .cornerRadius(20)
             
-            Text("Vividh Bharati  FM")
+            Text(currentFM)
                 .font(.title2)
                 .fontWeight(.semibold)
             
             Text(currentTime)
-                .font(.system(size: 36, weight: .bold, design: .monospaced))
+                .font(.system(size: 26, weight: .bold, design: .monospaced))
                 .padding()
                 .onReceive(timer) { _ in
                     currentTime = Self.getCurrentTime()
                 }
             
-            ModernSlider(
-                "Volume",
+            ModernSlider("",
                 systemImage: "speaker.wave.2.fill",
+                sliderHeight: 20,
                 value: $volume,
                 in: 0...1,
                 onChange: { newValue in
@@ -58,7 +57,7 @@ struct RadioPlayerView: View {
                 },
                 onChangeEnd: { finalValue in
                     self.volume = finalValue
-                    self.player?.volume = Float(finalValue)
+                    playerManager.player?.volume = Float(finalValue)
                 }
             )
             
@@ -67,9 +66,9 @@ struct RadioPlayerView: View {
                 
                 Button(action: {
                     if isPlaying {
-                        pauseRadio()
+                        playerManager.pauseRadio()
                     } else {
-                        playRadio()
+                        playerManager.playRadio()
                     }
                     isPlaying.toggle()
                 }) {
@@ -84,9 +83,9 @@ struct RadioPlayerView: View {
                 
                 Button(action: {
                     if isPlaying {
-                        pauseRadio()
+                        playerManager.pauseRadio()
                     } else {
-                        playRadio()
+                        playerManager.playRadio()
                     }
                     isPlaying.toggle()
                 }) {
@@ -104,23 +103,32 @@ struct RadioPlayerView: View {
             Text("Your FM List")
                 .font(.headline)
                 .padding(.top)
-            
+            let radioList = playerManager.getReadioList()
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 15) {
-                    ForEach(["Kannada FM", "Hindi FM", "Marati FM", "Telagu FM"], id: \.self) { station in
+                HStack(spacing: 10) {
+                    ForEach(radioList) { station in
                         VStack {
-                            Image(systemName: "music.quarternote.3")
-                                .resizable()
-                                .frame(width: 40, height: 40)
-                                .padding()
-                                .background(LinearGradient(gradient: Gradient(colors: [.pink, .orange]), startPoint: .top, endPoint: .bottom))
-                                .cornerRadius(15)
-                            
-                            Text(station)
+                            Button(action: {
+                                playerManager.changeStream(to: station.url, volume: volume)
+                                playerManager.playRadio()
+                                currentFM = station.station
+                            }) {
+                                Image(systemName: station.imageName)
+                                    .resizable()
+                                    .frame(width: 40, height: 40)
+                                    .padding()
+                                    .background(LinearGradient(gradient: Gradient(colors: [.green, .blue]), startPoint: .top, endPoint: .bottom))
+                                    .cornerRadius(15)
+                            }
+                            Text(station.radioName)
                                 .font(.subheadline)
-                            Text("00.00")
+                                .foregroundColor(.white)
+                            
+                            Text(station.station)
                                 .font(.caption)
                                 .foregroundColor(.white)
+                                .lineLimit(nil)
+                                .multilineTextAlignment(.center)
                         }
                         .frame(width: 100)
                     }
@@ -131,22 +139,11 @@ struct RadioPlayerView: View {
             Spacer()
         }
         .padding()
-        .background(LinearGradient(gradient: Gradient(colors: [.pink, .orange]), startPoint: .topLeading, endPoint: .bottomTrailing))
+        .background(LinearGradient(gradient: Gradient(colors: [.pink, .blue, .orange]), startPoint: .topLeading, endPoint: .bottomTrailing))
         .foregroundColor(.white)
         .onAppear {
-            let url = URL(string: "https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8")!
-            self.player = AVPlayer(url: url)
-            self.player?.volume = Float(self.volume)
+            playerManager.changeStream(to: "https://25083.live.streamtheworld.com/RADIO_SUNO_MELODY_S06.mp3", volume: volume)
         }
-    }
-    
-    
-    func playRadio() {
-        player?.play()
-    }
-    
-    func pauseRadio() {
-        player?.pause()
     }
     
     static func getCurrentTime() -> String {
@@ -159,10 +156,18 @@ struct RadioPlayerView: View {
 class RadioPlayerManager: ObservableObject {
     @Published var player: AVPlayer?
     @Published var isPlaying = false
+    @Published var currentURL: String?
+    @Published var volume : Double = 0.9
     
-    init() {
-        let url = URL(string: "https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8")!
-        player = AVPlayer(url: url)
+    init(_ url: String) {
+        self.currentURL = url
+        setupPlayer(with: url, volume: volume)
+    }
+    
+    func setupPlayer(with url: String, volume: Double) {
+        guard let streamURL = URL(string: url) else { return }
+        player = AVPlayer(url: streamURL)
+        self.player?.volume = Float(volume)
     }
     
     func playRadio() {
@@ -174,13 +179,70 @@ class RadioPlayerManager: ObservableObject {
         player?.pause()
         isPlaying = false
     }
+    
+    func changeStream(to newURL: String, volume: Double) {
+        player = nil
+        isPlaying = false
+        currentURL = nil
+        currentURL = newURL
+        setupPlayer(with: newURL, volume: volume)
+    }
+    
+    func getReadioList() -> [RadioModel] {
+        return [
+                RadioModel(imageName: "music.quarternote.3",
+                           radioName: "Kannada FM",
+                           station: "Suno Melody",
+                           url: "https://25083.live.streamtheworld.com/RADIO_SUNO_MELODY_S06.mp3"),
+                RadioModel(imageName: "music.microphone.circle.fill",
+                           radioName: "Hindi FM",
+                           station: "Vividh Bharati",
+                           url: "https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8"),
+                RadioModel(imageName: "radio",
+                           radioName: "Radio City",
+                           station: "91.1 FM",
+                           url: "https://stream.zeno.fm/pxc55r5uyc9uv"),
+                RadioModel(imageName: "music.note.tv",
+                           radioName: "Radio Mirchi",
+                           station: "98.3 FM",
+                           url: "https://18093.live.streamtheworld.com/NJS_HIN_ESTAAC/HLS/5b01b0d3-75bc-4d43-8908-ed743ed30007/0/playlist.m3u8"),
+                RadioModel(imageName: "apple.haptics.and.music.note",
+                           radioName: "Radio Mango",
+                           station: "91.9 FM",
+                           url: "https://eu10.fastcast4u.com/clubfmuae"),
+                RadioModel(imageName: "music.mic",
+                           radioName: "Air FM Gold",
+                           station: "खरा सोना (Real Gold)",
+                           url: "https://airhlspush.pc.cdn.bitgravity.com/httppush/hlspbaudio005/hlspbaudio005_Auto.m3u8"),
+                RadioModel(imageName: "music.note",
+                           radioName: "Raagam Radio",
+                           station: "Anytime Indian",
+                           url: "https://uk2.internet-radio.com/proxy/desiradio?mp=/stream"),
+                RadioModel(imageName: "antenna.radiowaves.left.and.right",
+                           radioName: "Namm Radio",
+                           station: "Kannada Kelo Majaane Bere",
+                           url: "https://stream.zeno.fm/6quh1pfnt1duv"),
+                RadioModel(imageName: "dot.radiowaves.left.and.right",
+                           radioName: "All India Radio",
+                           station: "AIR Dharwad",
+                           url: "https://air.pc.cdn.bitgravity.com/air/live/pbaudio150/playlist.m3u8")
+            ]
+    }
 }
+
+
+struct RadioModel : Identifiable {
+    let id = UUID()
+    let imageName: String
+    let radioName: String
+    let station: String
+    let url: String
+     
+}
+
 
 struct RadioPlayerView_Previews: PreviewProvider {
     static var previews: some View {
         RadioPlayerView()
     }
 }
-
-// "https://air.pc.cdn.bitgravity.com/air/live/pbaudio001/playlist.m3u8"
-// http://peridot.streamguys.com:7150/Mirchi
